f67b535aa061a0c8e3aa8927663883f0cfdf68d8
anjanaaneelayathh <dope79939>
2019-07-19 22:48:37 +0530

    Update Day-4.js

--- Task-1/Day-4.js
+++ Task-1/Day-4.js
@@ -1,1 +1,23 @@
+/*1.Create an array,a,of 10 numbers from 0 to 9.
+=>*/
+let a=[0,1,2,3,4,5,6,7,8,9];
+
+console.log(a[4]); //a)prints the fifth element from array defined.
+
+a.push("10","11","12","13","14","15","16","17","18","19","20");
+//b)Insert numbers 10 to 20 in array a.
+a.pop();//c) Remove the last element from the array.
+a.shift();//d)Removes the first element of defined array.
+//e)adds an element at the beginning of the array.
+
+ 
+
+
+
+
+
+
+
+
+
 
