5ddd128ae151697bf6172ecb7221adf565c18255
anjanaaneelayathh <dope79939>
2019-07-20 21:47:37 +0530

    Update Day-4.js

--- Task-1/Day-4.js
+++ Task-1/Day-4.js
@@ -1,1 +1,58 @@
 
+//this is question 1
+let a=[0,1,2,3,4,5,6,7,8,9];
+console.log(a[5]);
+a.push(10,11,12,13,14,15,16,17,18,19,20);//inserts all the elements in the array
+console.log(a);
+a.pop();  // removes last entered element
+console.log(a);
+a.shift(); // removes element from the start of the array
+console.log(a);
+a.unshift(21);//inserts 21 at the start of the array
+console.log(a);
+console.log(a.indexOf(17)); //gives index of element 17
+a.splice(11,1);//removes the item at index 11
+console.log(a);
+a.splice(4,5);//removes 5 items  starting from index 4
+console.log(a);
+
+
+let b=[];
+for( let i = 0; i < a.length; i++){ 
+    b[i]=a[i]*i;
+}
+console.log(b);
+
+
+
+
+
+
+
+
+
+
+let c=[];
+for( let i = 0; i < b.length; i++){ 
+    if(b[i]>30){
+        c.push(b[i]);
+    }
+}
+console.log(c);
+
+
+
+console.log(...a,...b,...c);
+
+
+
+
+
+
+
+
+
+        
+    
+
+
