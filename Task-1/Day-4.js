fb7faf71399fdfdd8a265148133c57ad09482a18
anjanaaneelayathh <dope79939>
2019-07-20 22:34:20 +0530

    Update Day-4.js

--- Task-1/Day-4.js
+++ Task-1/Day-4.js
@@ -18,25 +18,27 @@ console.log(a);
 
 
 let b=[];
-for( let i = 0; i < a.length; i++){ 
+for( let i = 0; i < a.length; i++){ //array b created
     b[i]=a[i]*i;
 }
 console.log(b);
 
-console.log(a.slice(7,11));
+
+
+console.log(a.slice(7,11));//elements from 7 to 10 index displayed
 
 
 let c=[];
 for( let i = 0; i < b.length; i++){ 
     if(b[i]>30){
-        c.push(b[i]);
+        c.push(b[i]);//array c created
     }
 }
 console.log(c);
 
 
 
-console.log(...a,...b,...c);
+console.log(...a,...b,...c);//this merges array a,b,c
 
 
 
