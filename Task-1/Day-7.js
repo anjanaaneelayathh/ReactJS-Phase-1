f0d813064be8d74ed567dab175af1f2eee4a3030
anjanaaneelayathh <dope79939>
2019-07-20 20:04:41 +0530

    Update Day-7.js

--- Task-1/Day-7.js
+++ Task-1/Day-7.js
@@ -1,1 +1,23 @@
+//this is question 4
+class Animal{
+    constructor(legs){
+        this.legs=legs;
 
+    };
+    getlegs(){
+        return this.legs;
+    };
+
+}
+class Dog extends Animal{   //extends keword is used  to inherit a property from
+    constructor(legs)       //parent class to child class,here the property inherited is legs.
+    {
+        super(legs)
+        this.voice="bowbow";
+    }
+    getVoice(){
+        return this.voice;
+    }
+}
+a=new Dog(4,"abc","xyz");
+console.log(a.getVoice()+"\n");
