b1515f81e80546cf11ccedefe121db8fa9fe2f35
anjanaaneelayathh <dope79939>
2019-07-20 23:09:48 +0530

    Update Day-4.js

--- Task-1/Day-4.js
+++ Task-1/Day-4.js
@@ -17,10 +17,9 @@ a.splice(4,5);//removes 5 items  starting from index 4
 console.log(a);
 
 
-let b=[];
-for( let i = 0; i < a.length; i++){ //array b created
-    b[i]=a[i]*i;
-}
+let b=a.map((value,index)=>{
+    return value*index;
+});
 console.log(b);
 
 
@@ -28,17 +27,13 @@ console.log(b);
 console.log(a.slice(7,11));//elements from 7 to 10 index displayed
 
 
-let c=[];
-for( let i = 0; i < b.length; i++){ 
-    if(b[i]>30){
-        c.push(b[i]);//array c created
-    }
-}
-console.log(c);
+let c=b.filter((value)=>{
+    return value>30;         //array c created
+});
 
 
-
-console.log(...a,...b,...c);//this merges array a,b,c
+let x=[...a,...b,...c];
+console.log(x);//this merges array a,b,c
 
 
 
