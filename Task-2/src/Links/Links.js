2235836fa7c857492684bd9f76e760eb5bdbcccc
anjanaaneelayathh <52846103+anjanaaneelayathh@users.noreply.github.com>
2019-08-03 18:21:02 +0530

    Create Links.js

---
+++ Task-2/src/Links/Links.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Links.css';
+
+
+const Links = ()=>{
+    return(
+
+
+<div>
+   <div className='font'>IMPORTANT LINKS</div>
+   
+      <div align='right'className='links'><button className='button'>NAAC</button>
+      <button className='button'>RULES</button>
+      <button className='button'>FEEDBACK</button>
+      <button className='button'>MANDATORY DISCLOSURE</button>
+      <button className='button'>NIRF</button>
+      <button className='button'>IIIC</button> <br></br>
+      </div>
+       
+   
+</div>
+  );
+}
+export default Links;
