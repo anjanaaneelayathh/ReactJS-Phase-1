8272a002ba5ffb6396f75cbf2810a5edec751077
anjanaaneelayathh <52846103+anjanaaneelayathh@users.noreply.github.com>
2019-08-03 18:09:36 +0530

    Update Card1.js

--- Task-2/src/Card/Card1.js
+++ Task-2/src/Card/Card1.js
@@ -1,1 +1,23 @@
 
+import React from 'react';
+import './Card1.css';
+
+const Card=(props)=>{
+    let {cardtitle,cardinfo,date,branch}=props;
+
+    return(
+        <div class='card'>
+          <img style={{    width: '255px' ,height: '170px'}}src='http://www.siesgst.edu.in/assets/background1.jpg'/>
+          <p class='cardtitle'>{cardtitle}</p>  
+          <span class='cardinfo'>{cardinfo}</span>
+          <div style={{display:'flex',flexdirection:'row',justifyContent:'space-around'}}>
+              <div class='datebranch' >{date}</div>
+              <div class='datebranch' >{branch}</div> 
+          </div>  
+        </div>
+        
+    );
+
+}
+
+export default Card;
