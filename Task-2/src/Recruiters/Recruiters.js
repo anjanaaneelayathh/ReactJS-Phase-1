3eafef4f7f3cdaca338c4d5a7dc6055ac8dd3cda
anjanaaneelayathh <52846103+anjanaaneelayathh@users.noreply.github.com>
2019-08-03 18:18:24 +0530

    Create Recruiters.js

---
+++ Task-2/src/Recruiters/Recruiters.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Recruiters.css'
+
+const Recruiters=()=>{
+    return(
+        <div style={{marginLeft:'150px'}}>
+  <h3>Top Recruiters</h3>
+  <hr></hr>
+  <div class="row"> 
+  <div class="column">
+  <img src="http://www.siesgst.edu.in/assets/company/Top/techmahindra.png"/>
+    <img src="http://www.siesgst.edu.in/assets/company/Top/zte.png"/>
+    <img src="http://www.siesgst.edu.in/assets/company/Top/tcs.png"/>
+    <img src="http://www.siesgst.edu.in/assets/company/Top/l&t.png"/>
+
+  </div> 
+  <br></br>
+
+  <div class="column">
+  <img src="http://www.siesgst.edu.in/assets/company/Top/dabur.png"/>
+    <img src="http://www.siesgst.edu.in/assets/company/Top/gobain.png"/>
+    <img src="http://www.siesgst.edu.in/assets/company/Top/ibm.png"/>
+    <img src="http://www.siesgst.edu.in/assets/company/Top/jacobs.png"/>
+
+  </div>
+  <br></br>
+
+
+  <div class="column">
+  <img src="http://www.siesgst.edu.in/assets/company/Top/oracle.png"/> 
+  <img src="http://www.siesgst.edu.in/assets/company/Top/cadbury.png"/>
+    <img src="http://www.siesgst.edu.in/assets/company/Top/capgemini.png"/>
+
+  <img src="http://www.siesgst.edu.in/assets/company/Top/jsw.png"/>
+  </div>
+</div>
+        </div>
+    );
+}
+export default Recruiters;
