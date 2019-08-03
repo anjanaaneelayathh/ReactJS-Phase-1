ecdf4896a05e7677c2db3ad684db376aa154118b
anjanaaneelayathh <52846103+anjanaaneelayathh@users.noreply.github.com>
2019-08-03 16:58:51 +0530

    Create Marquee.js

---
+++ Task-2/src/Header/Marquee.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Marquee.css';
+
+const Marquee = ()=>{
+    return(
+
+
+<div><marquee behavior="scroll" direction="left" ><tab className='black'><tab className="tab">NEW</tab>&nbsp;&nbsp;INSTITUTE LEVEL MERIT LIST RELEASED</tab></marquee></div>
+  );
+}
+export default Marquee;
