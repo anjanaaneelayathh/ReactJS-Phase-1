3917d6afa01d07d9f27ffa7d89cf62c1290ac103
anjanaaneelayathh <52846103+anjanaaneelayathh@users.noreply.github.com>
2019-08-03 18:23:05 +0530

    Create Events.js

---
+++ Task-2/src/Events/Events.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './Events.css';
+
+const Upcoming=()=>{
+    return(
+        <div style={{paddingLeft:'10px'}}>
+            <h3>Upcoming Events</h3>
+            <hr></hr>
+            <ul>
+                <li class="upcomingbox">
+                    No new Events
+                </li>
+            </ul>
+            
+
+
+        </div>
+    );
+}
+
+export default Upcoming;
