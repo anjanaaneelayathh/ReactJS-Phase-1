ead69a869e0b02176176c2c3a95ef4707340dfbb
anjanaaneelayathh <52846103+anjanaaneelayathh@users.noreply.github.com>
2019-08-03 18:19:37 +0530

    Create Lifehere.js

---
+++ Task-2/src/Lifehere/Lifehere.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import './Lifehere.css';
+
+
+const Lifehere = ()=>{
+    return(
+
+
+<div>
+<div class="container">
+    <h5 class="text-uppercase" id="gstlife">
+        A glipms of Life@GST
+
+            </h5>
+    <h6 class="grey-text">Events and workshops for your extracurricular growth </h6>
+
+
+    <div class="divider"></div>
+    <section class="section">
+        <div class="grid">
+            <div class="item">
+                <div class="item__details">
+                   Our Technical Fest - Cognition
+                </div>
+            </div>
+            <div class="item item--large">
+                <div class="item__details">
+                    Celebrities joining us for our events
+                </div>
+            </div>
+            <div class="item item--medium">
+                <div class="item__details">
+                    Annual sports meet - Lakshya
+                </div>
+            </div>
+            <div class="item item--large">
+                <div class="item__details">
+                    Developers Weekend SIESGST
+                </div>
+            </div>
+            <div class="item item--full">
+                <div class="item__details">
+                    EDM night during our annual festival TML (Tatva Moksha Lakshya )
+                </div>
+            </div>
+            <div class="item item--medium">
+                <div class="item__details">
+                    TEDxSIESGST
+                </div>
+            </div>
+            <div class="item">
+                <div class="item__details">
+                    Bytecamp our official hackathon
+                </div>
+            </div>
+
+        </div>
+    </section>
+</div>
+
+
+</div>
+  );
+}
+export default Lifehere;
