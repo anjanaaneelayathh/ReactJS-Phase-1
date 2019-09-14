641b666a40b04338ee6415ec9fbffad280664eff
anjanaaneelayathh <52846103+anjanaaneelayathh@users.noreply.github.com>
2019-09-15 05:12:42 +0530

    Update Header.js

--- Task-3&4/siesportal/src/Header/Header.js
+++ Task-3&4/siesportal/src/Header/Header.js
@@ -1,1 +1,42 @@
+import React,{Component} from 'react';
+import './Header.css';
+import SidePanel from './SideDrawer/SideDrawer';
 
+class Header extends Component  {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          showMenu: false,
+        };
+        this.closeMenu = this.closeMenu.bind(this);
+        this._onButtonClick = this._onButtonClick.bind(this);
+      }
+    
+      _onButtonClick() {
+        this.setState({
+          showMenu: true,
+        }, () => {
+          document.addEventListener('click', this.closeMenu);
+          console.log('OnButtonClick')
+        });
+      }
+      closeMenu() {
+        this.setState({ showMenu: false }, () => {
+          document.removeEventListener('click', this.closeMenu);
+          console.log('closeMenu')
+    
+        });
+      }
+    
+      render() {
+        return (
+              <div class='headd'>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+                <i style={{paddingLeft:'20px'}} class="fa fa-bars fa-lg" onClick={this._onButtonClick}></i>
+                <span class = 'textheadd'>SIESGST Portal</span>
+                {this.state.showMenu ? <div ><SidePanel/></div> : null }
+              </div>
+        );}
+}
+export default Header;
