ed9fa13432100a817622d4db8c2f603f00657217
anjanaaneelayathh <52846103+anjanaaneelayathh@users.noreply.github.com>
2019-09-15 05:32:04 +0530

    Create Footer.js

---
+++ Task-3&4/siesportal/src/Footer/Footer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './Footer.css'
+
+function Footer (){
+        return (
+            <footer class="page-footer grey lighten-5">
+  <div class="footer-copyright grey-text text-darken-2">
+  
+
+      
+        <div class="greyy">
+          © 2018 Copyright SIESGST
+        </div>
+        
+      
+    
+  </div>
+</footer>
+        )
+}
+
+ 
+export default Footer;
