df1babb7259649a8f9d780dcfd22e60cee44b352
anjanaaneelayathh <52846103+anjanaaneelayathh@users.noreply.github.com>
2019-09-16 22:11:11 +0530

    Update Signin.js

--- Task-3&4/siesportal/src/Login/Signin/Signin.js
+++ Task-3&4/siesportal/src/Login/Signin/Signin.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button';
+import { NavLink } from 'react-router-dom'; 
 
 
 
 
-const Signin=()=>{
+const Signin =()=> {
     return(
         <Form style={{marginTop:'5%',padding:'25px',boxShadow:'0 16px 24px 2px rgba(0,0,0,.14), 0 6px 30px 5px rgba(0,0,0,.12), 0 8px 10px -7px rgba(0,0,0,.2)',height:'25%',width:'50%',marginLeft:'25%'}}>
+    <div>
+  
+    </div>
   <Form.Group controlId="formBasicEmail">
     <Form.Label style={{padding: '12px 12px 12px 0',display: 'inline-block'}}>Email address</Form.Label>
     <Form.Control type="email" placeholder="Enter email" />
@@ -15,6 +19,7 @@ const Signin=()=>{
       We'll never share your email with anyone else.
     </Form.Text>
   </Form.Group>
+  
   <Form.Group controlId="formBasicPassword">
     <Form.Label>Password</Form.Label>
     <Form.Control type="password" placeholder="Password" />
@@ -22,10 +27,11 @@ const Signin=()=>{
   <Form.Group controlId="formBasicCheckbox">
     <Form.Check type="checkbox" label="Check me out" />
   </Form.Group>
-    <Button variant="primary" type="submit">
-    Submit
-  </Button>
+  <Form.Group>
+  <Button variant="dark" type ="submit">Submit</Button>
+  </Form.Group>
 </Form>
     );
 }
+
 export default Signin;
